0ed95227c8ebd25b73d9090bcdcc8d011f698c34
parkerspeares <parker.speares@outlook.com>
2024-11-21 12:14:38 -0800

    Client
    interfaces components api

---
+++ client/src/api/authAPI.tsx
@@ -0,0 +1,16 @@
+import { UserLogin } from "../interfaces/UserLogin";
+
+const login = async (userInfo: UserLogin) => {
+  // TODO: make a POST request to the login route
+  const response = await fetch('/api/auth/login', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(userInfo),
+  });
+  const data = await response.json();
+  return data;
+}
+
+
+
+export { login };
